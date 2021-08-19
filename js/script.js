024927ae364ad4026aea229a5a3daf7c156992dd
Truong Thanh <61083809+thanhopvn@users.noreply.github.com>
2021-08-19 11:06:20 +0700

    update

--- js/script.js
+++ js/script.js
@@ -16,3 +16,20 @@ $('.about-us').slick({
     autoplay: true,
     autoplaySpeed: 2000,
 });
+//Tab
+function openCity(evt, productName) {
+    var i, titlep, product;
+    titlep = document.getElementsByClassName("product");
+    for (i = 0; i < titlep.length; i++) {
+      titlep[i].style.display = "none";
+    }
+    product = document.getElementsByClassName("title-p");
+    for (i = 0; i < product.length; i++) {
+      product[i].className = product[i].className.replace(" active", "");
+    }
+    document.getElementById(productName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
